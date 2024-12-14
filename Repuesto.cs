4a2c1f3d77d08c9df1496754fceeea9a0473555b
sntg120898 <51084864+sntg120898@users.noreply.github.com>
2024-12-13 23:09:47 -0500

    Add files via upload

---
+++ Repuesto.cs
@@ -0,0 +1,18 @@
+﻿namespace TallerRepuestosASP.Models
+{
+    public class Repuesto
+    {
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+
+        public void ActualizarCantidad(int cantidadVendida)
+        {
+            if (cantidadVendida <= Cantidad)
+            {
+                Cantidad -= cantidadVendida;
+            }
+        }
+    }
+}
