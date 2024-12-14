d5de59440ed3f40c125247dc868decc5e9c6b538
sntg120898 <51084864+sntg120898@users.noreply.github.com>
2024-12-13 23:09:22 -0500

    Add files via upload

---
+++ Vehiculo.cs
@@ -0,0 +1,16 @@
+﻿namespace TallerRepuestosASP.Models
+{
+    using System.Collections.Generic;
+
+    public class Vehiculo
+    {
+        public string Modelo { get; set; }
+        public string Tipo { get; set; }
+        public List<Repuesto> Repuestos { get; set; } = new List<Repuesto>();
+
+        public void AgregarRepuesto(Repuesto repuesto)
+        {
+            Repuestos.Add(repuesto);
+        }
+    }
+}
