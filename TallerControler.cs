eb1c843a43086ba33cc7a3b6c6af063024804d53
sntg120898 <51084864+sntg120898@users.noreply.github.com>
2024-12-13 23:08:35 -0500

    Add files via upload

---
+++ TallerControler.cs
@@ -0,0 +1,33 @@
+﻿namespace TallerRepuestosASP.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using TallerRepuestosASP.Models;
+
+    public class TallerController : Controller
+    {
+        private static Taller _taller = new Taller();
+
+        public IActionResult Index()
+        {
+            return View(_taller);
+        }
+
+        public IActionResult AgregarRepuesto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AgregarRepuesto(Repuesto repuesto)
+        {
+            _taller.AgregarRepuesto(repuesto);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult BuscarRepuesto(string termino)
+        {
+            var resultados = _taller.BuscarRepuesto(termino);
+            return View(resultados);
+        }
+    }
+}
